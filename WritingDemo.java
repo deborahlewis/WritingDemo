f15def9164d94f3ab3a317287e4caa663f277a9d
Deborah Lewis <deborahlewis@users.noreply.github.com>
2016-08-20 15:48:20 -0400

    Add files via upload

--- WritingDemo.java
+++ WritingDemo.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Deborah Lewis
+ * This program is used to test writing to files
  */
 package writingdemo;
 
@@ -10,22 +9,27 @@ import java.io.File; //import to create file
 import java.io.FileWriter; //import for FileWriter
 import java.io.IOException;
 
-/**
- *
- * @author Deborah
- */
+
 public class WritingDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws IOException {
-        File testWriter = new File("People.txt");//create file
-        BufferedWriter out = new BufferedWriter(new FileWriter(testWriter, true));  //open file & write
         
-        out.write("What happens now"); //write "Test" to file
-        out.newLine();        
-        out.close(); //save
-    }
-    
-}
+        Person p = new Person();
+        p.setFirstName("Deborah");
+        p.setLastName("Lewis");
+        p.setAge(25);
+        
+        File testWriter = new File ("People.txt"); //create file if not already in existence
+        BufferedWriter out = new BufferedWriter(new FileWriter(testWriter, true)); //open file
+        
+        out.write(p.getFirstName()); //write to file
+        out.write(", ");
+        out.write(p.getLastName());
+        out.write(", ");
+        out.write("" + p.getAge()); //write number not character representation of the number
+        out.newLine(); //new line
+        out.close(); //save & close file
+ 
+        
+    } //end main    
+} //end class
